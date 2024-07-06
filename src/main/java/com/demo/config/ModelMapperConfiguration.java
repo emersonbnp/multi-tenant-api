49ee84a4853fa9a66f966da5d2ec2f9087290dab
emersonbnp <emersonbnp@gmail.com>
2024-07-06 08:43:09 -0300

    Adds API configurations

---
+++ src/main/java/com/demo/config/ModelMapperConfiguration.java
@@ -0,0 +1,19 @@
+package com.demo.config;
+
+import com.bhub.challenge.mapper.DeliveryNoteRequestToDTOConverter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfiguration {
+
+  @Bean
+  public ModelMapper modelMapper() {
+    ModelMapper mapper = new ModelMapper();
+    mapper.getConfiguration().setSkipNullEnabled(true);
+    mapper.addConverter(new DeliveryNoteRequestToDTOConverter());
+    return mapper;
+  }
+
+}
